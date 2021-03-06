c389a2ed9b019e762a8f9408c4b72a4164dc1bed
sQUEsSs7QfWg4yc <cosmmy02@outlook.es>
2021-03-06 23:16:29 +0000

    Definición de prefix añadida junto a un comentario indicativo

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const Discord = require('discord.js');
 const keepAlive = require('./server.js');
 
 const client = new Discord.Client();
+const prefix = process.env.prefix;
+
+// Tu código aquí
 
 keepAlive();
 client.login(process.env.token);
